ff1f0836a7fe473d36f3aaae2639fc5cf0e008f6
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-11-06 22:33:16 +0600

    Task 2

--- Lab_09/JDBC/src/Task2/Main.java
+++ Lab_09/JDBC/src/Task2/Main.java
@@ -1,7 +1,50 @@
 package Task2;
 
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
+
+        String username = "C_210042174";
+        String password = "cse4308";
+        String url = "jdbc:oracle:thin:@localhost:1521:xe";
+
+        try {
+            // Register JDBC driver
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+            // Create the connection object
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+            // Create the statement object
+            PreparedStatement preparedStatement1 = connection.prepareStatement ("INSERT INTO TRANSACTIONS VALUES (? ,? ,? ,?, ?)") ;
+            preparedStatement1.setInt (1 , 10001); ;
+            preparedStatement1.setDate (2 , new Date(2022, 2, 12)); ;
+            preparedStatement1.setInt (3 , 2); ;
+            preparedStatement1.setDouble (4 , 5000.0); ;
+            preparedStatement1.setString (5 , "1") ;
+            preparedStatement1.executeUpdate () ;
+
+            PreparedStatement preparedStatement2 = connection.prepareStatement ("INSERT INTO TRANSACTIONS VALUES (? ,? ,? ,?, ?)") ;
+            preparedStatement2.setInt (1 , 10005); ;
+            preparedStatement2.setDate (2 , new Date(2022, 10, 15)); ;
+            preparedStatement2.setInt (3 , 4); ;
+            preparedStatement2.setDouble (4 , 10000.0); ;
+            preparedStatement2.setString (5 , "0") ;
+            preparedStatement2.executeUpdate () ;
+
+            System.out.println("Data inserted successfully!");
+
+            // Close the connection
+            preparedStatement1.close();
+            preparedStatement2.close();
+            connection.close();
+
+        } catch (SQLException e) {
+            System.out.println("Connection to DB failed. Exception: " + e);
+        } catch (ClassNotFoundException e) {
+            System.out.println("JDBC driver not found. Exception: " + e);
+        }
 
     }
 }
